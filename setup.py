344ea98a1e7a921d3ad63db237d445f42ab58158
Bruno Alla <bruno.alla@festicket.com>
2020-03-31 19:42:08 +0100

    Drop Python 2.0 support

--- setup.py
+++ setup.py
@@ -64,7 +64,6 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
